6141a284a104861b6403ff9647bb707bd582d104
Skkay <skkay.thomas@gmail.com>
2022-01-18 16:05:03 +0100

    Add /logout route

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -22,4 +22,12 @@ class LoginController extends AbstractController
             'error'         => $error,
         ]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
