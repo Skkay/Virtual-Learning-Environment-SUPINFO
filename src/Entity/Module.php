b552590661b43defcc7917a415d96af6bc21a78c
Skkay <skkay.thomas@gmail.com>
2022-01-24 20:32:47 +0100

    Add Module entity

---
+++ src/Entity/Module.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ModuleRepository;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=ModuleRepository::class)
+ * @UniqueEntity(fields={"label"}, message="There is already a module with this label")
+ */
+class Module
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $label;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Intervenant::class, mappedBy="modules")
+     */
+    private $intervenants;
+
+    public function __construct()
+    {
+        $this->intervenants = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Intervenant[]
+     */
+    public function getIntervenants(): Collection
+    {
+        return $this->intervenants;
+    }
+
+    public function addIntervenant(Intervenant $intervenant): self
+    {
+        if (!$this->intervenants->contains($intervenant)) {
+            $this->intervenants[] = $intervenant;
+            $intervenant->addModule($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIntervenant(Intervenant $intervenant): self
+    {
+        if ($this->intervenants->removeElement($intervenant)) {
+            $intervenant->removeModule($this);
+        }
+
+        return $this;
+    }
+}
