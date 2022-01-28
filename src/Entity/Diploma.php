9392ebd2b5edf59fcbad46c5494158be427034d6
Skkay <skkay.thomas@gmail.com>
2022-01-28 19:55:40 +0100

    Add Note entity

--- src/Entity/Diploma.php
+++ src/Entity/Diploma.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\DiplomaRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Diploma
      */
     private $label;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Etudiant::class, mappedBy="lastDiploma")
+     */
+    private $etudiants;
+
+    public function __construct()
+    {
+        $this->etudiants = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,36 @@ class Diploma
     public function setLabel(string $label): self
     {
         $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Etudiant[]
+     */
+    public function getEtudiants(): Collection
+    {
+        return $this->etudiants;
+    }
+
+    public function addEtudiant(Etudiant $etudiant): self
+    {
+        if (!$this->etudiants->contains($etudiant)) {
+            $this->etudiants[] = $etudiant;
+            $etudiant->setLastDiploma($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEtudiant(Etudiant $etudiant): self
+    {
+        if ($this->etudiants->removeElement($etudiant)) {
+            // set the owning side to null (unless already changed)
+            if ($etudiant->getLastDiploma() === $this) {
+                $etudiant->setLastDiploma(null);
+            }
+        }
 
         return $this;
     }
