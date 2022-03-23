149bce5c10f9509cd5a9167d8ad1328a471e0d12
Skkay <skkay.thomas@gmail.com>
2022-03-23 20:42:19 +0100

    Add '/campus/{id}' route

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
 use App\Repository\CampusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,6 +28,16 @@ class CampusController extends AbstractController
         $campus = $this->campusRepository->findAll();
 
         return $this->render('campus/index.html.twig', [
+            'campus' => $campus,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show")
+     */
+    public function show(Campus $campus): Response
+    {
+        return $this->render('campus/show.html.twig', [
             'campus' => $campus,
         ]);
     }
