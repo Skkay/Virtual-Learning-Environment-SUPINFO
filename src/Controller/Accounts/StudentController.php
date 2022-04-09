ddcb3ee1d9f7819cbfacb9e1d8c2efc828bd475d
Skkay <skkay.thomas@gmail.com>
2022-04-09 16:28:47 +0200

    (accounts) Add student documents list

--- src/Controller/Accounts/StudentController.php
+++ src/Controller/Accounts/StudentController.php
@@ -5,8 +5,12 @@ namespace App\Controller\Accounts;
 use App\Entity\Student;
 use App\Repository\StudentRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,13 +20,17 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class StudentController extends AbstractController
 {
+    private $studentDocumentsDirectories;
+    private $logger;
     private $em;
 
     /** @var StudentRepository */
     private $studentRepository;
 
-    public function __construct(ManagerRegistry $doctrine)
+    public function __construct(ParameterBagInterface $params, LoggerInterface $logger, ManagerRegistry $doctrine)
     {
+        $this->studentDocumentsDirectories = $params->get('app.accounts.student_documents_directories');
+        $this->logger = $logger;
         $this->em = $doctrine->getManager();
         $this->studentRepository = $this->em->getRepository(Student::class);
     }
@@ -46,6 +54,28 @@ class StudentController extends AbstractController
     {
         return $this->render('accounts/student/show.html.twig', [
             'student' => $student,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/documents", name="document")
+     */
+    public function getStudentDocuments(Student $student): Response
+    {
+        $filesystem = new Filesystem();
+        $finder = new Finder();
+
+        $studentDocumentsDirectory = $this->studentDocumentsDirectories . $student->getId();
+        if (!$filesystem->exists($studentDocumentsDirectory)) {
+            $filesystem->mkdir($studentDocumentsDirectory);
+
+            $this->logger->debug('Directory ' . $studentDocumentsDirectory . 'created');
+        }
+
+        $studentDocuments = $finder->files()->in($studentDocumentsDirectory)->sortByName(true);
+        return $this->render('accounts/student/documents.html.twig', [
+            'student' => $student,
+            'documents' => iterator_to_array($studentDocuments),
         ]);
     }
 }
