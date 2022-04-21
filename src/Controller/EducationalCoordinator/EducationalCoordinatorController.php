bfac1e0726ccffdf2ea17b4d8e091d595b4b3f1d
Skkay <skkay.thomas@gmail.com>
2022-04-21 20:43:52 +0200

    Add 'EducationalCoordinatorController'

---
+++ src/Controller/EducationalCoordinator/EducationalCoordinatorController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\EducationalCoordinator;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/educational_coordinator", name="app.educational_coordinator.")
+ * @Security("is_granted('ROLE_EDUCATIONAL_COORDINATOR')")
+ */
+class EducationalCoordinatorController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('educational_coordinator/index.html.twig', [
+            'controller_name' => 'EducationalCoordinatorController',
+        ]);
+    }
+}
