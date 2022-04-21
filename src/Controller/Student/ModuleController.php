f8c4bbe4196f87ee350b904c0a55421c1455f230
Skkay <skkay.thomas@gmail.com>
2022-04-21 19:57:12 +0200

    Move Student related controllers to \Student namespace

--- src/Controller/Student/ModuleController.php
+++ src/Controller/Student/ModuleController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Student;
 
 use App\Entity\Grade;
 use App\Entity\Module;
--- src/Controller/Student/ReportCardController.php
+++ src/Controller/Student/ReportCardController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Student;
 
 use App\Entity\Level;
 use App\Entity\Student;
