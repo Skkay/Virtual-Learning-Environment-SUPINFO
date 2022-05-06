af2228e7af0ef72d12f3912f85f8aa8b322474c4
Skkay <skkay.thomas@gmail.com>
2022-05-06 18:59:25 +0200

    Add 'full_calendar.js' to load FullCalendar

---
+++ assets/js/full_calendar.js
@@ -0,0 +1,29 @@
+import { Calendar } from '@fullcalendar/core';
+
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+import frLocale from '@fullcalendar/core/locales/fr';
+
+import '../styles/full_calendar.css';
+
+const calendarEl = document.getElementById('calendar');
+
+const calendar = new Calendar(calendarEl, {
+    events: calendarEl.getAttribute('data-url'),
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+    },
+    navLinks: true,
+    dayMaxEvents: true,
+    eventStartEditable: false,
+    locales: [frLocale],
+    locale: 'fr',
+});
+
+calendar.render();
