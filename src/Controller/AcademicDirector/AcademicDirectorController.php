9bba5db9790b577dee6dc221abf7ae367f0e5df7
Skkay <skkay.thomas@gmail.com>
2022-05-07 18:31:19 +0200

    Add 'AcademicDirectorController'

---
+++ src/Controller/AcademicDirector/AcademicDirectorController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\AcademicDirector;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/academic_director", name="app.academic_director.")
+ * @Security("is_granted('ROLE_ACADEMIC_DIRECTOR')")
+ */
+class AcademicDirectorController extends AbstractController
+{
+    /**
+     * @Route("", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('academic_director/index.html.twig', [
+            'controller_name' => 'AcademicDirectorController',
+        ]);
+    }
+}
