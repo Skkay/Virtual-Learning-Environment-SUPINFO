3ad6918111bd6dc64d5e8464d6e66ef433b4839d
Skkay <skkay.thomas@gmail.com>
2022-05-16 20:24:32 +0200

    Add partnership list

---
+++ src/Controller/AcademicDirector/PartnershipController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\AcademicDirector;
+
+use App\Entity\Partnership;
+use App\Repository\PartnershipRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/academic_director/partnership", name="app.academic_director.partnership.")
+ * @Security("is_granted('ROLE_ACADEMIC_DIRECTOR')")
+ */
+class PartnershipController extends AbstractController
+{
+    private $em;
+
+    /** @var PartnershipRepository */
+    private $partnershipRepository;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->em = $doctrine->getManager();
+        $this->partnershipRepository = $this->em->getRepository(Partnership::class);
+    }
+
+    /**
+     * @Route("", name="index")
+     */
+    public function index(): Response
+    {
+        $partnerships = $this->partnershipRepository->findAll();
+
+        return $this->render('academic_director/partnership/index.html.twig', [
+            'partnerships' => $partnerships,
+        ]);
+    }
+}
