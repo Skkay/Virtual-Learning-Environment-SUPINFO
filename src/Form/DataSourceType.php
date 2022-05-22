db2d12cb67832c776444c25274d114960947582c
Skkay <skkay.thomas@gmail.com>
2022-05-22 11:12:26 +0200

    (admin) Translation of data source pages

--- src/Form/DataSourceType.php
+++ src/Form/DataSourceType.php
@@ -5,17 +5,23 @@ namespace App\Form;
 use App\Entity\DataSource;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DataSourceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('label', TextType::class)
-            ->add('equivalence', TextType::class)
+            ->add('label', TextType::class, [
+                'label' => 'form.data_source_type.label',
+                'required' => true,
+            ])
+            ->add('equivalence', TextType::class, [
+                'label' => 'form.data_source_type.equivalence',
+                'required' => true,
+            ])
         ;
 
         $builder->get('equivalence')->addModelTransformer(new CallbackTransformer(
