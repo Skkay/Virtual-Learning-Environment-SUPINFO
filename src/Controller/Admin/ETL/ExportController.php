85b140a03cf860235fa8e7611a83e6a4684e4f9b
Skkay <skkay.thomas@gmail.com>
2022-05-30 14:05:38 +0200

    Add route to download an export

--- src/Controller/Admin/ETL/ExportController.php
+++ src/Controller/Admin/ETL/ExportController.php
@@ -3,9 +3,12 @@
 namespace App\Controller\Admin\ETL;
 
 use App\Service\ExportService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * @Route("/admin/etl/exports", name="app.admin.etl.export.")
@@ -13,10 +16,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ExportController extends AbstractController
 {
     private $exportService;
+    private $exportDirectory;
 
-    public function __construct(ExportService $exportService)
+    public function __construct(ExportService $exportService, ParameterBagInterface $params)
     {
         $this->exportService = $exportService;
+        $this->exportDirectory = $params->get('app.database.export_directory');
     }
 
     /**
@@ -29,5 +34,23 @@ class ExportController extends AbstractController
         return $this->render('admin/etl/export/index.html.twig', [
             'exports' => $exports,
         ]);
+    }
+
+    /**
+     * @Route("/{filename}", name="download_file")
+     */
+    public function downloadFile(string $filename): Response
+    {
+        $finder = new Finder();
+
+        $finder->files()->in($this->exportDirectory)->name($filename);
+
+        if (!$finder->hasResults()) {
+            throw new NotFoundHttpException('Requested file "' . $this->exportDirectory . '/' . $filename . '" not fond');
+        }
+
+        $filePath = iterator_to_array($finder, false)[0]->getRealPath();
+
+        return $this->file($filePath);
     }
 }
