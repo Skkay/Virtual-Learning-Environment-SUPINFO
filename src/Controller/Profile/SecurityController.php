55fcc86b99e486c9a3554799b1605d8c59664540
Skkay <skkay.thomas@gmail.com>
2022-06-05 20:24:02 +0200

    Add SecurityController and route to define a password

---
+++ src/Controller/Profile/SecurityController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller\Profile;
+
+use App\Entity\User;
+use App\Form\SetPasswordType;
+use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/me/security", name="app.profile.security.")
+ * @Security("is_granted('ROLE_USER')")
+ */
+class SecurityController extends AbstractController
+{
+    private $em;
+    private $passwordHaser;
+
+    public function __construct(ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHaser)
+    {
+        $this->em = $doctrine->getManager();
+        $this->passwordHaser = $passwordHaser;
+    }
+
+    /**
+     * @Route("/set_password", name="set_password")
+     */
+    public function index(Request $request): Response
+    {
+        /** @var User */
+        $user = $this->getUser();
+
+        if ($user->getPassword() !== null) {
+            // TODO
+        }
+
+        $form = $this->createForm(SetPasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $encodedPassword = $this->passwordHaser->hashPassword($user, $data['password']);
+            $user->setPassword($encodedPassword);
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+            $this->addFlash('password_set', 'profile.security.misc.password_set');
+
+            return $this->redirectToRoute('app.profile.index');
+        }
+
+        return $this->renderForm('profile/security/set_password.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
