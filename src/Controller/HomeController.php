d0a2716c35a158118827258ab6591b12568ea0cb
Skkay <skkay.thomas@gmail.com>
2022-06-07 14:31:39 +0200

    Clean HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\DataLoaderService;
-use App\Service\TestService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -49,28 +47,4 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
-
-    /**
-     * @Route("/test", name="app.test")
-     */
-    public function test(DataLoaderService $dataLoaderService): Response
-    {
-        $dataLoaderService->loadFiles();
-        // return $this->json(['response' => 200]);
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
-
-    // /**
-    //  * @Route("/test", name="app.test")
-    //  */
-    // public function test(TestService $testService): Response
-    // {
-    //     $testService->testExtractor();
-    //     // return $this->json(['response' => 200]);
-    //     return $this->render('home/index.html.twig', [
-    //         'controller_name' => 'HomeController',
-    //     ]);
-    // }
 }
